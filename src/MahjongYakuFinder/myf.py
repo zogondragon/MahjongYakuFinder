5bd7e3e35c62d918e23dc0727f259cf99927aeda
이건희/Network Automation그룹(네트워크)/Staff Engineer/삼성전자 <teuksu00@gmail.com>
2023-01-09 14:57:29 +0900

    Mahjong HandNotationConverter initial commit.

---
+++ src/MahjongYakuFinder/myf.py
@@ -0,0 +1,34 @@
+OrderOfCounts = ["1m", "2m", "3m", "4m", "5m", "6m", "7m", "8m", "9m",
+    "1s", "2s", "3s", "4s", "5s", "6s", "7s", "8s", "9s",
+    "1p", "2p", "3p", "4p", "5p", "6p", "7p", "8p", "9p",
+    "E", "S", "W", "N", "B", "G", "R"]
+
+class HandNotationConverter:
+    """This class converts between human readable text notation and
+    machine readable count notation."""
+    def ConvertTextToSetNotation(textNotationStr):
+        # The text notation should be sorted (m) -> (s) -> (p) -> (ESWNBGR) order.
+        if self.VerifyTextNotation(textNotationStr) == False:
+            print("Given hand is not well-ordered.")
+            return None
+        # is Man tile exist?
+
+        # is Sak tile exist?
+
+        # is Ping tile exist?
+
+        # is East South West North Blank Green Red tile exist?
+
+        pass
+
+    def ConvertSetToTextNotation(setNotationStr):
+        pass
+
+    def VerifyTextNotation(textNotationStr):
+
+
+class Hand:
+    """Every mahjong hand can be regarded as unordered sets. Every tile of
+    same kind is treated indifferently, thus we can just store the counts of
+    each kind of tiles for our purpose."""
+    pass
