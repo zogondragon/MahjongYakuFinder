143f5c9d55d4b6ece53641c9ac006dcf61b4f16d
이건희/Network Automation그룹(네트워크)/Staff Engineer/삼성전자 <teuksu00@gmail.com>
2023-01-10 14:35:22 +0900

    VerifyTextNotation() function implemented.

--- src/MahjongYakuFinder/myf.py
+++ src/MahjongYakuFinder/myf.py
@@ -1,3 +1,5 @@
+import re
+
 OrderOfCounts = ["1m", "2m", "3m", "4m", "5m", "6m", "7m", "8m", "9m",
     "1s", "2s", "3s", "4s", "5s", "6s", "7s", "8s", "9s",
     "1p", "2p", "3p", "4p", "5p", "6p", "7p", "8p", "9p",
@@ -9,7 +11,7 @@ class HandNotationConverter:
     def ConvertTextToSetNotation(self, textNotationStr):
         # The text notation should be sorted (m) -> (s) -> (p) -> (ESWNBGR) order.
         if self.VerifyTextNotation(textNotationStr) == False:
-            print("Given hand is not well-ordered.")
+            print("Given hand is not a proper hand.")
             return None
         # is Man tile exist?
 
@@ -25,7 +27,22 @@ class HandNotationConverter:
         pass
 
     def VerifyTextNotation(self, textNotationStr):
-        pass
+        # Check if not allowed character is present in the input string.
+        errorpattern = re.compile("[^1-9mspESWNBGR]")
+        if errorpattern.match(textNotationStr) is not None:
+            return False
+
+        # Check if the input string contains more or less than 14 tiles.
+        tileLength = len(textNotationStr.replace('m', '').replace('s', '').replace('p', ''))
+        if tileLength != 14:
+            return False
+
+        # Check the ordering and count of tiles.
+        pattern = re.compile("(1{0,4}2{0,4}3{0,4}4{0,4}5{0,4}6{0,4}7{0,4}8{0,4}9{0,4}m)?"+
+                             "(1{0,4}2{0,4}3{0,4}4{0,4}5{0,4}6{0,4}7{0,4}8{0,4}9{0,4}s)?"+
+                             "(1{0,4}2{0,4}3{0,4}4{0,4}5{0,4}6{0,4}7{0,4}8{0,4}9{0,4}p)?"+
+                             "E{0,4}S{0,4}W{0,4}N{0,4}B{0,4}G{0,4}R{0,4}")
+        return True if pattern.fullmatch(textNotationStr) is not None else False
 
 class Hand:
     """Every mahjong hand can be regarded as unordered sets. Every tile of
--- tests/test_myf.py
+++ tests/test_myf.py
@@ -11,6 +11,24 @@ class TestHandNotationConverter(unittest.TestCase):
     def test_ConvertTextToSetNotation(self):
         self.assertEqual(self.hnc.ConvertTextToSetNotation("123m123s123pEEESS"), 
             "1110000001110000001110000003200000")
+
+    def test_VerifyTextNotation(self):
+        self.assertEqual(self.hnc.VerifyTextNotation("slIGho328jhs@$!"), False)      # Gibberish string
+        self.assertEqual(self.hnc.VerifyTextNotation("11122233344455m"), True)       # Good hand
+        self.assertEqual(self.hnc.VerifyTextNotation("111112222233333m"), False)     # 15-tile hand
+        self.assertEqual(self.hnc.VerifyTextNotation("1111222233334m"), False)       # 13-tile hand
+        self.assertEqual(self.hnc.VerifyTextNotation("111122223333333m"), False)     # More than 4 tiles of 3m
+        self.assertEqual(self.hnc.VerifyTextNotation("5544433322211m"), False)       # Not sorted 
+        self.assertEqual(self.hnc.VerifyTextNotation("11122233344455s"), True)       # Good hand
+        self.assertEqual(self.hnc.VerifyTextNotation("11122233344455p"), True)       # Good hand
+        self.assertEqual(self.hnc.VerifyTextNotation("2478m111s899pESSR"), True)     # Good hand
+        self.assertEqual(self.hnc.VerifyTextNotation("2478m111s899pEGNR"), False)    # Not sorted
+        self.assertEqual(self.hnc.VerifyTextNotation("2478m11155s899pESSR"), False)  # 16-tile hand
+        self.assertEqual(self.hnc.VerifyTextNotation("2478p111s899mESSR"), False)    # Not sorted
+        self.assertEqual(self.hnc.VerifyTextNotation("ESSR2478m111s899p"), False)    # Not sorted
+        self.assertEqual(self.hnc.VerifyTextNotation("2478m11155m899mESSR"), False)  # Multiple Man tiles
+        self.assertEqual(self.hnc.VerifyTextNotation("2478s11155s899sESSR"), False)  # Multiple Sak tiles
+        self.assertEqual(self.hnc.VerifyTextNotation("2478s11155p899pESSR"), False)  # Multiple Ping tiles
             
 if __name__ == '__main__':
     unittest.main()
